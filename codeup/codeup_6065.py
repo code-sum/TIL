b1168985d8cce33171934604905372c604b75f8c
code-sum <yisugyeong@gmail.com>
2022-07-16 08:40:17 +0900

    Python Codeup 100제(65~66)

---
+++ codeup/codeup_6065.py
@@ -0,0 +1,14 @@
+a, b, c = input().split()
+
+a = int(a)
+b = int(b)
+c = int(c)
+
+if a%2==0 : 
+  print(a)
+
+if b%2==0 :
+  print(b) 
+
+if c%2==0 :
+  print(c) 
---
+++ codeup/codeup_6066.py
@@ -0,0 +1,20 @@
+a, b, c = input().split()
+
+a = int(a)
+b = int(b)
+c = int(c)
+
+if a%2==0 :
+  print("even")
+else :
+  print("odd") 
+
+if b%2==0 :
+  print("even")
+else :
+  print("odd") 
+
+if c%2==0 :
+  print("even")
+else :
+  print("odd") 
