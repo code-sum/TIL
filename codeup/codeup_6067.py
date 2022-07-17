4db696c6415148ca334d943936fb882aae71dfe3
code-sum <yisugyeong@gmail.com>
2022-07-17 22:41:55 +0900

    Python Codeup 100제(67~68)

---
+++ codeup/codeup_6067.py
@@ -0,0 +1,15 @@
+n = input()
+
+n = int(n)
+
+if n < 0:
+    if n % 2 == 0:
+        print('A')
+    else:
+        print('B')
+if n > 0:
+    if n % 2 == 0:
+        print('C')
+    else:
+        print('D')
+
---
+++ codeup/codeup_6068.py
@@ -0,0 +1,11 @@
+n = input()
+n = int(n)
+
+if n <= 100 and n >= 90:
+    print('A')
+elif n >= 70:
+    print('B')
+elif n >= 40:
+    print('C')
+elif n >= 0:
+    print('D')
