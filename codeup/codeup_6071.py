6c1014d7f7f4bfcf1fbb9053df570950adb9e60a
code-sum <yisugyeong@gmail.com>
2022-07-26 22:03:21 +0900

    Python Codeup 100제(71)

---
+++ codeup/codeup_6071.py
@@ -0,0 +1,9 @@
+# https://codeup.kr/problem.php?id=6071
+
+
+n = 1    # n의 초기값을 임의로 define(꼭 1이 아니어도 됨)
+
+while n != 0:    # n이 0이 아닌한 작업 반복한다
+    n = int(input())
+    if n != 0:
+        print(n)
