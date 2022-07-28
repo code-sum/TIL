2b6cfe7b877fbac09c53c02949eea36e3fa0142f
code-sum <yisugyeong@gmail.com>
2022-07-28 21:54:44 +0900

    Python Codeup 100제(73~74)

---
+++ codeup/codeup_6074.py
@@ -0,0 +1,9 @@
+alpha = ord(input())
+start = ord('a')
+
+while start <= alpha:
+    print(chr(start), end = '')
+    start += 1
+
+# 알파벳 문자 a의 정수값은 ord('a')로 알아낼 수 있다.
+# chr(정수값)을 이용하면 다시 유니코드 문자를 출력할 수 있다.
